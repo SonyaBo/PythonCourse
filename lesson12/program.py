d6d2ae4582a5ef1d6537c61308e021d21ff77966
Sofia <sofiaborodkina@gmail.com>
2025-03-30 14:48:13 +0200

    decorators added

--- lesson12/program.py
+++ lesson12/program.py
@@ -1,19 +1,40 @@
 import time
 
-"""
-start_time = time.time()
-result = method(*args)
-end_time = time.time()
-print(f"Time taken: {method.__name__}: {end_time - start_time:.4f} sec")"""
-def timer_decorator(method):
+counter = {}
+
+
+def handler(default):
+    def exception_handler(method):
         def wrapper(*args):
-            start_time = time.time()
-            result = method(*args)
-            end_time = time.time()
-            print(f"Time taken: {method.__name__}: {end_time - start_time} sec")
-            return result
+            try:
+                return method(*args)
+            except Exception:
+                return default
 
         return wrapper
+
+    return exception_handler
+
+
+def timer_decorator(method):
+    def wrapper(*args):
+        start_time = time.time()
+        result = method(*args)
+        end_time = time.time()
+        print(f"Time taken: {method.__name__}: {end_time - start_time} sec")
+        return result
+
+    return wrapper
+
+
+def block_call(method):
+    def wrapper(*args):
+        if counter[method.__name__] >= 4:
+            return "Limit of calls reached."
+        else:
+            return method(*args)
+    return wrapper()
+
 
 def repeater_decorator(n):
     def repeat_n_times_decorator(method):
@@ -21,10 +42,12 @@ def repeater_decorator(n):
             for i in range(n):
                 result = method(*args)
             return result
+
         return wrapper
+
     return repeat_n_times_decorator
 
-counter = {}
+
 def count_func_repeat(method):
     def wrapper(*args):
         if method.__name__ in counter:
@@ -33,17 +56,26 @@ def count_func_repeat(method):
             counter[method.__name__] = 1
         print(f"{method.__name__}: {counter[method.__name__]} times")
         return method(*args)
+
     return wrapper
 
+
 class FileReader:
-    @repeater_decorator(n=4)
+
     @count_func_repeat
-    @timer_decorator
-    def read_file(self,file):
+    def read_file(self, file):
         filer = ''
-        with open(file,"r") as f:
+        with open(file, "r") as f:
             filer = f.read()
         return filer
 
+
 fr = FileReader()
-fr.read_file("some.txt")
+
+fr.read_file()
+
+fr.read_file()
+fr.read_file()
+fr.read_file()
+print(counter)
+print(fr.read_file())
